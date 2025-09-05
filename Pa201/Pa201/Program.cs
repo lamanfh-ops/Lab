40c2785459f2adc41b4161ee33bb081c8d5b2b92
Laman F. Hajizada <lamanfh@code.local>
2025-09-05 15:34:01 +0400

    first commit

---
+++ Pa201/Pa201/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//string name = "Leman";
+
+//Console.WriteLine("Hello " + name);
+
+int age = 1;
+
+if(age < 18 && age >0)
+{
+    Console.WriteLine("Usaq");
+}
+else if(age <= 65)
+{
+    Console.WriteLine("yetiskin");
+}
+else
+{
+    Console.WriteLine("Yasli");
+}
